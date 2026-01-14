6d2743e17c7d312b989337e50a3027612a7ad936
iTsMot9 <itsmot9s@gmail.com>
2026-01-14 17:25:37 +0300

    postgres-fixfiles

--- postgres/init-scripts/20-create-databases.sh
+++ postgres/init-scripts/20-create-databases.sh
@@ -5,7 +5,7 @@ export VARIANT="v3"
 export SCRIPT_PATH=/docker-entrypoint-initdb.d/
 export PGPASSWORD=postgres
 
-psql --dbname template1 -f "$SCRIPT_PATH/scripts/db-$VARIANT.sql"
+psql --dbname template1 -f "$SCRIPT_PATH/db-$VARIANT.sql"
 
 psql -U program -d cars -f "$SCRIPT_PATH/schemas/schema-3.sql"
 psql -U program -d rentals -f "$SCRIPT_PATH/schemas/schema-3.sql"
